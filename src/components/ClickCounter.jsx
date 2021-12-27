d40a7da514b8047f1ed60080854eaca012ba6ca7
vijayender46 <69912576+vijayender46@users.noreply.github.com>
2021-12-27 02:53:22 +0000

    Added click functionality and tests

--- src/components/ClickCounter.jsx
+++ src/components/ClickCounter.jsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import reactDom from "react-dom";
 
 export const ClickCounter = () => {
-     
-    // const [count, setCount] = React.useState(0);
+    let styles = {
+        color :'green',
+    }
+     const [count, setCount] = React.useState(0);
     
     return(
         <div className="section" data-test-id="counter-wrapper">
-            <h1 data-test-id="count-display">0</h1>
-            <button data-test-id="count-button">Click</button>
+            <h1 data-test-id="count-display">Counter is at &nbsp;
+                <span style={styles} data-test-id="count">{count}</span>
+            </h1>
+            <button
+            onClick={() => setCount(count + 1)}
+            data-test-id="count-button">Click</button>
         </div> 
     )
 } 
--- src/components/ClickCounter.test.js
+++ src/components/ClickCounter.test.js
@@ -6,8 +6,52 @@ import { ClickCounter } from './ClickCounter';
 
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
+// DRY code process
+/**
+ * Factory function to create shallow wrapper to component we going to test.
+ */
+const setup = () => shallow(<ClickCounter />);
+/**
+ * Factory function to create shallow wrapper to component we going to test.
+ */
+const findTestAttr = (wrapper, val) => wrapper.find(`[data-test-id='${val}']`);
+
 test('component renders without errors', () => {
-    const wrapper = shallow(<ClickCounter />);
-    const counterComponent = wrapper.find("[data-test-id='counter-wrapper']");
+    const wrapper = setup();
+    const counterComponent = findTestAttr(wrapper, 'counter-wrapper');
     expect(counterComponent.length).toBe(1);
 })
+
+// renders count display text
+test('renders count initial', () => {
+    const wrapper = setup();
+    const counterDisplay = findTestAttr(wrapper, 'count-display');
+    expect(counterDisplay.length).toBe(1);
+});
+
+// display count button
+test('renders count button', () => {
+    const wrapper = setup();
+    const countButton = findTestAttr(wrapper, 'count-button');
+    expect(countButton.length).toBe(1);
+});
+
+// renders count value 0 as initial
+test('renders count initial', () => {
+    const wrapper = setup();
+    const countValue = findTestAttr(wrapper, 'count').text();
+    expect(countValue).toBe('0');
+});
+
+// click funtion works
+test('click function increament count', () => {
+    const wrapper = setup();
+    // find the button
+    const countButton = findTestAttr(wrapper, 'count-button');
+    // click happened - to get click we use .simulate from enzyme doc
+    countButton.simulate('click');
+
+    // find display and find if count happen on click
+    const countValue = findTestAttr(wrapper, 'count').text();
+    expect(countValue).toBe('1');
+});
